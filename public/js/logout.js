e65bbe59190f1919692a70a306b21f68eccc313d
xiongxeng <xiongxeng@gmail.com>
2023-12-17 11:48:30 -0600

    add notes, edit seeders to add names

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,15 +1,18 @@
-const logout = async () => {
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
+// const logout = async () => {
+    //const response = await fetch('/api/users/logout', {
+      //method: 'POST',
+      //headers: { 'Content-Type': 'application/json' },
+    //});
   
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  };
+    //if (response.ok) {
+      //document.location.replace('/');
+    //} else {
+     // alert(response.statusText);
+    //}
+  //};
   
-  document.querySelector('#logout').addEventListener('click', logout);
+  //document.querySelector('#logout').addEventListener('click', logout);
+
+
+  // Can delete this log out file, logout will be handle in the controller/middleware/auth.js file
   
---
+++ public/js/manager.js
@@ -0,0 +1,10 @@
+// Manage the data of managers
+
+// Inside the functions should include the following:
+// 1. set event handlers which redirect to the corresponding routes
+// 2. Prevents the submission of the default form
+// 3. Handles the form submission
+// 4. Show alert message in alert.js
+      // error: (msg) => {
+        // showAlert(msg.statusText)
+        // }
